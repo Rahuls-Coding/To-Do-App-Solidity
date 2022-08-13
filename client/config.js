a272d85374e40e0819c035090691c1925807de5a
Rahul <rahulscodingworld@gmail.com>
2022-08-13 17:45:01 +0530

    finished connect wallet button

---
+++ client/config.js
@@ -0,0 +1,2 @@
+export const TaskContractAddress = '0x119e9Af014c533d2e61741a9c102b214B2816954'
+
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,10 @@
 import WrongNetworkMessage from '../components/WrongNetworkMessage'
 import ConnectWalletButton from '../components/ConnectWalletButton'
 import TodoList from '../components/TodoList'
+import {useState} from 'react'
+import { TaskContract } from '../config.js'
+import TaskAbi from '../../backend/build/contracts/TaskContract.json'
+import { ethers } from "ethers"
 
 /* 
 const tasks = [
@@ -12,9 +16,41 @@ const tasks = [
 
 export default function Home() {
 
+  const [correctNetwork, setCorrectNetwork] = useState(false)
+  const [isUserConnected, setIsUserConnected] = useState(false)
+  const [currentAccount, setCurrentAccount] = useState('')
+
+
+
   // Calls Metamask to connect wallet on clicking Connect Wallet button
   const connectWallet = async () => {
+    try {
+      const { ethereum } = window
+      if (!ethereum) {
+        console.log('No ethereum is detected')
+        return
+      }
+      let chainId = await ethereum.request({ method: 'eth_chainId' })
+      console.log('connected to', chainId)
+      const renkebyChain = '0x4'
 
+      if (chainId !== renkebyChain) {
+        alert("Wrong network")
+        setCorrectNetwork(false)
+        return
+      } else {
+        setCorrectNetwork(true)
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts'})
+      console.log('accounts', accounts[0])
+      setCurrentAccount(accounts[0])
+      setIsUserConnected(true)
+
+    } 
+    catch (error) {
+      console.log(error)
+    }
   }
 
   // Just gets all the tasks from the contract
@@ -24,6 +60,7 @@ export default function Home() {
 
   // Add tasks from front-end onto the blockchain
   const addTask = async e => {
+    e.preventDefault()
 
   }
 
@@ -34,8 +71,8 @@ export default function Home() {
 
   return (
     <div className='bg-[#97b5fe] h-screen w-screen flex justify-center py-6'>
-      {!'is user not logged in?' ? <ConnectWalletButton /> :
-        'is this the correct network?' ? <TodoList /> : <WrongNetworkMessage />}
+      {!isUserConnected ? <ConnectWalletButton connectWallet={connectWallet} /> :
+        !correctNetwork ? <TodoList /> : <WrongNetworkMessage />}
     </div>
   )
 }
